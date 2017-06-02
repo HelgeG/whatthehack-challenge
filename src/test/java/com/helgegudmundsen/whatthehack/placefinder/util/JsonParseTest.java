2463621a0b80d1cf42208ff89735074ce7bfd713
Helge Gudmundsen <helge@spotqa.com>
2017-06-02 23:02:25 +0100

    Added test that reads results from Json file

---
+++ src/test/java/com/helgegudmundsen/whatthehack/placefinder/util/JsonParseTest.java
@@ -0,0 +1,32 @@
+package com.helgegudmundsen.whatthehack.placefinder.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import static org.junit.Assert.*;
+import org.junit.*;
+/**
+ * Reads a file with Json results and parses it. Created by helge on 02/06/2017.
+ */
+public class JsonParseTest {
+    @Test
+    public void readResultsFromJson()
+            throws IOException {
+        String json = new String(Files.readAllBytes(Paths.get("result.json")), StandardCharsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(json);
+
+        ArrayNode resultsNode = (ArrayNode) node.get("results");
+        assertTrue("We should have four results here" ,resultsNode.size() == 4);
+
+    }
+
+}
