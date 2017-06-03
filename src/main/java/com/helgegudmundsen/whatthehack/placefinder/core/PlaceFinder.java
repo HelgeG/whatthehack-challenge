0118c9e0241fd3126dce428104c9117299d9f068
Helge Gudmundsen <helge@spotqa.com>
2017-06-03 01:49:51 +0100

    Reformatting

--- src/main/java/com/helgegudmundsen/whatthehack/placefinder/core/PlaceFinder.java
+++ src/main/java/com/helgegudmundsen/whatthehack/placefinder/core/PlaceFinder.java
@@ -13,15 +13,25 @@ public class PlaceFinder {
     public static void main(String[] args) {
         Configuration config = new Configuration(Configuration.VERSION_2_3_26);
         config.setClassForTemplateLoading(PlaceFinder.class, "/");
-        //get("/beer", (req, res) -> "Hello World, Time for a beer!");
 
         try {
-            Template lookForTemplate = config.getTemplate("lookfor.ftl");
-            StringWriter writer = new StringWriter();
-            Map<String, Object> map = new HashMap<>();
-            map.put("type", "Beer");
-            lookForTemplate.process(map, writer);
-            get("/beer", (request, response) -> writer);
+            get("/beer", (request, response) -> {
+                Template lookForTemplate = config.getTemplate("lookfor.ftl");
+                StringWriter writer = new StringWriter();
+                Map<String, Object> map = new HashMap<>();
+                map.put("type", "Beer");
+                lookForTemplate.process(map, writer);
+                return writer;
+            });
+            post("/find", (request, response) -> {
+                Template lookForTemplate = config.getTemplate("results.ftl");
+                StringWriter writer = new StringWriter();
+                Map<String, Object> map = new HashMap<>();
+                map.put("type", "Beer");
+                lookForTemplate.process(map, writer);
+                return writer;
+            });
+
         } catch (Exception e) {
             halt(500);
             e.printStackTrace();
