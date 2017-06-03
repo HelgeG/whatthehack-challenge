4fb767073cafab60e10ca85826b9e6d0a5ccec80
Helge Gudmundsen <helge@spotqa.com>
2017-06-03 01:28:06 +0100

    Introducing freemarker template

--- src/main/java/com/helgegudmundsen/whatthehack/placefinder/core/PlaceFinder.java
+++ src/main/java/com/helgegudmundsen/whatthehack/placefinder/core/PlaceFinder.java
@@ -1,9 +1,30 @@
 package com.helgegudmundsen.whatthehack.placefinder.core;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class PlaceFinder {
     public static void main(String[] args) {
-        get("/beer", (req, res) -> "Hello World, Time for a beer!");
+        Configuration config = new Configuration(Configuration.VERSION_2_3_26);
+        config.setClassForTemplateLoading(PlaceFinder.class, "/");
+        //get("/beer", (req, res) -> "Hello World, Time for a beer!");
+
+        try {
+            Template lookForTemplate = config.getTemplate("lookfor.ftl");
+            StringWriter writer = new StringWriter();
+            Map<String, Object> map = new HashMap<>();
+            map.put("type", "Beer");
+            lookForTemplate.process(map, writer);
+            get("/beer", (request, response) -> writer);
+        } catch (Exception e) {
+            halt(500);
+            e.printStackTrace();
+        }
     }
 }
